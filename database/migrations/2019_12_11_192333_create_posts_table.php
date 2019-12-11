423122220a46fdcdce1cebdf7eb0dbcf3e8164a2
Vlad <kapagaev111@gmail.com>
2019-12-11 21:31:11 +0200

    syntax fix

--- database/migrations/2019_12_11_192333_create_posts_table.php
+++ database/migrations/2019_12_11_192333_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('content');
             $table->timestamps();
+        });
     }
 
     /**
